f4c7a6c25216eb0fa4c94234decbde8edea614a9
owhenrique <me.pauloalmeida@gmail.com>
2023-07-19 17:34:15 -0300

    fix: change footer icons

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { AiFillInstagram, AiFillFacebook, AiOutlinePhone } from "react-icons/ai";
-import { HiOutlineMail } from "react-icons/hi";
+import { AiFillInstagram, AiFillFacebook, AiFillPhone } from "react-icons/ai";
+import { HiMail } from "react-icons/hi";
 
 function Footer() {
   return (
@@ -28,13 +28,13 @@ function Footer() {
         <ul className="my-2 flex flex-col gap-2">
           <li>
             <a href="" className="flex gap-2">
-              <HiOutlineMail size={24} color="white" />
+              <HiMail size={24} color="white" />
               <span className="text-gray-400">contato@spadasalmas.com</span>
             </a>
           </li>
           <li>
             <a href="" className="flex gap-2">
-              <AiOutlinePhone size={24} color="white" />
+              <AiFillPhone size={24} color="white" />
               <span className="text-gray-400">+55 (032) 3333-3333</span>
             </a>
           </li>
